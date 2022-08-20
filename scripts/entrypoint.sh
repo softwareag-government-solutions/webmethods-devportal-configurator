81c8f3a6edc0881d1deb89faa2b93ab4110fb5eb
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-08-20 18:01:43 -0400

    adding user management functions

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,13 +10,28 @@ fi
 logger_with_headers $LOGGER_INFO "Starting Developer Portal Configurator..."
 
 ## wait for devportal connect - always first!
-if [ "${devportal_wait_connect}" == "true" ]; then
+if [ "${wait_connect}" == "true" ]; then
     exec_ansible_playbook wait_connect.yaml
 fi
 
 ## update_password
-if [ "${devportal_changepassword_enabled}" == "true" ]; then
+if [ "${changepassword_enabled}" == "true" ]; then
     exec_ansible_playbook update_password.yaml
+fi
+
+## config users
+if [ "${settings_users_configure}" == "true" ]; then
+    exec_ansible_playbook config_users.yaml
+fi
+
+## config user groups
+if [ "${settings_usergroups_configure}" == "true" ]; then
+    exec_ansible_playbook config_usergroups.yaml
+fi
+
+## config roles
+if [ "${settings_userroles_configure}" == "true" ]; then
+    exec_ansible_playbook config_userroles.yaml
 fi
 
 logger_with_headers $LOGGER_INFO "Developer Portal Configurator Done !!"
